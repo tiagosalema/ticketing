dd489013f228588b55fb1ba04b5878ea13bfc4e5
tiagosalema <tiagof.salema@gmail.com>
2021-09-15 19:54:07 +0100

    other routes added

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentUser', () => {});
+router.get('/api/users/currentUser', (req, res) => {
+  res.send('his');
+});
 
 export { router as currentUserRouter };
